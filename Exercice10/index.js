11248d1b9e9cf3da304a9217d52bf91e247ca2bd
aalexandree-g <aalexandree.g@gmail.com>
2025-03-17 16:32:11 +0100

    Exercice 10 done

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,10 @@
-//votre code ici
+function printNumbers(n) {
+    n = String(n)
+    let nombre = "1"
+    for (let i = 2; i <= n; i++) {
+        nombre += " " + i
+    }
+    return nombre
+}
 
 export default printNumbers
